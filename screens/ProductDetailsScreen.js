b61c243229bc20abfc60f6278bc28e9bea514930
DarkLord239 <husseinib23920@gmail.com>
2022-11-14 00:17:52 +0200

    Setting up the product detail screen

--- screens/ProductDetailsScreen.js
+++ screens/ProductDetailsScreen.js
@@ -1,12 +1,25 @@
-import { Text , View, ScrollView, StyleSheet } from "react-native";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
+
+import { useSelector } from "react-redux";
 
 const ProductDetail = props => {
 
+    const productId = props.route.params.prodId;
+    const selectedProduct = useSelector(state => state.products.availabelProducts.find(prod => prod.id == productId))
 
+    useLayoutEffect(() => {
+
+        const productTitle = selectedProduct.title;
+        props.navigation.setOptions({
+            title: productTitle
+        })
+
+    }, [ productId ,props.navigation])
 
     return (
         <View>
-            <Text> sdfsahdgiuas shgdasuldg budilaylowhrn</Text>
+            <Text>{selectedProduct.title}</Text>
         </View>
     )
 }
