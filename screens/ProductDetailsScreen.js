15cefcdb55b22f4917edc502d55f369a691ef153
DarkLord239 <husseinib23920@gmail.com>
2022-11-19 22:17:48 +0200

    Outputting contents on Details Screen

--- screens/ProductDetailsScreen.js
+++ screens/ProductDetailsScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, ScrollView, StyleSheet } from "react-native";
+import { Text, View, ScrollView, StyleSheet, Image, Button } from "react-native";
 import { useLayoutEffect } from "react";
 
 import { useSelector } from "react-redux";
@@ -15,13 +15,51 @@ const ProductDetail = props => {
             title: productTitle
         })
 
-    }, [ productId ,props.navigation])
+    }, [productId, props.navigation])
 
     return (
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+        <ScrollView>
+            <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+            <View style={styles.buttonContainer} >
+                <Button title="Add to cart" />
+            </View>
+            <View style={styles.details} >
+                <Text style={styles.price} >${selectedProduct.price.toFixed(2)} </Text>
+            </View>
+            <View style={styles.description}>
+                <Text style={styles.descriptionText} >{selectedProduct.description}</Text>
+            </View>
+        </ScrollView>
     )
 }
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        marginVertical: 15,
+    },
+    details: {
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+    },
+    description:{
+        alignItems: 'center',
+        marginHorizontal: 25,
+        marginVertical: 20,
+    },
+    descriptionText:{
+        textAlign:'center',
+        fontSize:18,
+    },
+});
 
 export default ProductDetail;
