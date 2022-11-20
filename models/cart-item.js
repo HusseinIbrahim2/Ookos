9eccc9cc90aad3a828394d695bf335e3f6f66e21
DarkLord239 <husseinib23920@gmail.com>
2022-11-20 22:06:42 +0200

    Create constructor for cart item

---
+++ models/cart-item.js
@@ -0,0 +1,10 @@
+class Cart_Item {
+    constructor(quantity, productPrice, productTitle, sum) {
+        this.quantity = quantity;
+        this.productPrice = productPrice;
+        this.productTitle = productTitle;
+        this.sum = sum;
+    }
+}
+
+export default Cart_Item;
