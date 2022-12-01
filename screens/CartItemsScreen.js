d3ae6bcd14fd81f3ed33bcbfce67c47d05325c9d
DarkLord239 <husseinib23920@gmail.com>
2022-12-01 23:40:32 +0200

    Editing items in CartItems Screen

--- screens/CartItemsScreen.js
+++ screens/CartItemsScreen.js
@@ -1,9 +1,62 @@
-import { Text } from "react-native";
+import { Button, Text, View, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 
 const CartItemsScreen = props => {
+
+    const cartTotalAmount = useSelector(state => state.cart.totalAmount);
+    const cartItems = useSelector(state => {
+        const transformedCartItems = [];
+        for (const key in state.cart.items) {
+            transformedCartItems.push({
+                productId: key,
+                productTitle: state.cart.items[key].productTitle,
+                productPrice: state.cart.items[key].productPrice,
+                quantity: state.cart.items[key].quantity,
+                sum: state.cart.items[key].sum,
+            })
+        }
+        return transformedCartItems;
+    })
+
     return (
-        <Text>This is the cart item screen</Text>
+        <View style={styles.screen}>
+            <View style={styles.summary}>
+                <Text style={styles.summaryText}>
+                    Total: <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
+                </Text>
+                <Button color='#48480e' title="Order Now" disabled={cartItems.length == 0} />
+            </View>
+            <View>
+                <Text>Cart Items</Text>
+            </View>
+        </View>
+
     )
 }
+const styles = StyleSheet.create({
+    screen: {
+        margin: 20,
+    },
+    summary: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    summaryText: {
+        fontSize: 18,
+    },
+    amount: {
+        color: '#7d3a3a',
+    },
+})
 
 export default CartItemsScreen;
