426149fe7792234972727bfc64ed3ac486439ba0
DarkLord239 <husseinib23920@gmail.com>
2022-12-01 23:00:25 +0200

    Create CartItem Screen

---
+++ screens/CartItemsScreen.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+const CartItemsScreen = props => {
+    return (
+        <Text>This is the cart item screen</Text>
+    )
+}
+
+export default CartItemsScreen;
