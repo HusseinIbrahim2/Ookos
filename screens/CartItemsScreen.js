f380a26449680027af2ce21845889fa0794d4c0b
DarkLord239 <husseinib23920@gmail.com>
2022-12-02 21:03:32 +0200

    Editing the sort of  the items on removing them

--- screens/CartItemsScreen.js
+++ screens/CartItemsScreen.js
@@ -17,7 +17,7 @@ const CartItemsScreen = props => {
                 sum: state.cart.items[key].sum,
             })
         }
-        return transformedCartItems;
+        return transformedCartItems.sort((a,b) => a.productId > b.productId ? 1 : -1 );
     });
     const dispatch = useDispatch();
 
