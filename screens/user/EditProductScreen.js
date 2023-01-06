824ca9a4b77a7f7c29dd25ae704447f2d2af987e
DarkLord239 <husseinib23920@gmail.com>
2023-01-06 20:43:50 +0200

    Create edit product screnn

---
+++ screens/user/EditProductScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View , Text} from "react-native";
+
+const EditProductScreen = props => {
+    return (
+        <View>
+            <Text>Edit svrennnnnnnnnnnn</Text>
+        </View>
+    )
+}
+
+export  default EditProductScreen;
